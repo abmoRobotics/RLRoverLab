3847fedf08fa67655c1a47134e1b2c22b56d194f
abmoRobotics <antonbm2008@gmail.com>
2025-03-05 23:20:05 +0100

    Add new AAURoverEnvSimple environment registration

--- rover_envs/envs/navigation/robots/aau_rover/__init__.py
+++ rover_envs/envs/navigation/robots/aau_rover/__init__.py
@@ -24,6 +24,17 @@ gym.register(
 )
 
 gym.register(
+    id="AAURoverEnvSimple-v0",
+    entry_point='rover_envs.envs.navigation.entrypoints:RoverEnv',
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": env_cfg.AAURoverEnvCfg,
+        "best_model_path": f"{os.path.dirname(__file__)}/policies/best_agent2.pt",
+        "get_agent_fn": get_agent,
+    }
+)
+
+gym.register(
     id="AAURoverEnvCamera-v0",
     entry_point='rover_envs.envs.navigation.entrypoints:RoverEnv',
     disable_env_checker=True,
