04796a2289a8588a046634a253d3e317d080acf2
abmoRobotics <antonbm2008@gmail.com>
2025-03-09 02:21:26 +0100

    Add SKRL configuration paths for PPO, TRPO, TD3, SAC, and RPO in AAU Rover environment

--- rover_envs/envs/navigation/robots/aau_rover/__init__.py
+++ rover_envs/envs/navigation/robots/aau_rover/__init__.py
@@ -31,6 +31,13 @@ gym.register(
         "env_cfg_entry_point": env_cfg.AAURoverEnvCfgSimple,
         "best_model_path": f"{os.path.dirname(__file__)}/policies/best_agent2.pt",
         "get_agent_fn": get_agent,
+        "skrl_cfgs": {
+            "PPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_ppo.yaml",
+            "TRPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_trpo.yaml",
+            "TD3": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_td3.yaml",
+            "SAC": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_sac.yaml",
+            "RPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_rpo.yaml",
+        },
     }
 )
 
