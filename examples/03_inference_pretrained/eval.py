4ffcdfa36f54191cd1bda05cc6c2827d77f78be4
abmoRobotics <antonbm2008@gmail.com>
2025-05-20 14:54:27 +0200

    Add dataset arguments to eval script and remove deprecated record script

--- examples/03_inference_pretrained/eval.py
+++ examples/03_inference_pretrained/eval.py
@@ -18,6 +18,9 @@ parser.add_argument("--task", type=str, default="AAURoverEnvSimple-v0", help="Na
 parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
 parser.add_argument("--agent", type=str, default="PPO", help="Name of the agent.")
 parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint to resume training.")
+parser.add_argument("--dataset_dir", type=str, default="./datasets", help="Path to the dataset directory.")
+parser.add_argument("--dataset_name", type=str, default=None, help="Name of the dataset.")
+parser.add_argument("--dataset_type", type=str, default="RL", choices=["IL", "RL"], help="Type of dataset to use. Options: IL or RL.")
 
 AppLauncher.add_app_launcher_args(parser)
 args_cli, hydra_args = parser.parse_known_args()
@@ -42,88 +45,26 @@ from isaaclab.utils.dict import print_dict  # noqa: E402
 from isaaclab.utils.io import dump_pickle, dump_yaml  # noqa: E402
 
 
-def video_record(
-        env: ManagerBasedRLEnv, log_dir: str, video: bool, video_length: int, video_interval: int
-) -> ManagerBasedRLEnv:
-    """
-    Function to check and setup video recording.
-
-    Note:
-        Copied from the ORBIT framework.
-
-    Args:
-        env (ManagerBasedRLEnv): The environment.
-        log_dir (str): The log directory.
-        video (bool): Whether or not to record videos.
-        video_length (int): The length of the video (in steps).
-        video_interval (int): The interval between video recordings (in steps).
-
-    Returns:
-        ManagerBasedRLEnv: The environment.
-    """
-
-    if video:
-        video_kwargs = {
-            "video_folder": os.path.join(log_dir, "videos"),
-            "step_trigger": lambda step: step % video_interval == 0,
-            "video_length": video_length,
-        }
-        print("[INFO] Recording videos during training.")
-        print_dict(video_kwargs, nesting=4)
-        return gym.wrappers.RecordVideo(env, **video_kwargs)
-
-    return env
-
-
-def log_setup(experiment_cfg, env_cfg, agent):
-    """
-    Setup the logging for the experiment.
-
-    Note:
-        Copied from the ORBIT framework.
-    """
-    # specify directory for logging experiments
-    log_root_path = os.path.join(
-        "logs", "skrl", experiment_cfg["agent"]["experiment"]["directory"])
-    log_root_path = os.path.abspath(log_root_path)
-    print(f"[INFO] Logging experiment in directory: {log_root_path}")
-
-    # specify directory for logging runs
-    log_dir = datetime.now().strftime("%b%d_%H-%M-%S")
-    if experiment_cfg["agent"]["experiment"]["experiment_name"]:
-        log_dir = f'_{experiment_cfg["agent"]["experiment"]["experiment_name"]}'
-
-    log_dir += f"_{agent}"
-
-    # set directory into agent config
-    experiment_cfg["agent"]["experiment"]["directory"] = log_root_path
-    experiment_cfg["agent"]["experiment"]["experiment_name"] = log_dir
-
-    # update log_dir
-    log_dir = os.path.join(log_root_path, log_dir)
-
-    # dump the configuration into log-directory
-    dump_yaml(os.path.join(log_dir, "params", "env.yaml"), env_cfg)
-    dump_yaml(os.path.join(log_dir, "params", "agent.yaml"), experiment_cfg)
-    dump_pickle(os.path.join(log_dir, "params", "env.pkl"), env_cfg)
-    dump_pickle(os.path.join(log_dir, "params", "agent.pkl"), experiment_cfg)
-    return log_dir
-
-
 from isaaclab_tasks.utils import parse_env_cfg  # noqa: E402
 from skrl.trainers.torch import SequentialTrainer  # noqa: E402
 from skrl.utils import set_seed  # noqa: E402, F401
 
 import rover_envs  # noqa: E402
 import rover_envs.envs.navigation.robots  # noqa: E402, F401
+from rover_envs.utils.logging_utils import video_record, log_setup, configure_datarecorder  # noqa: E402
 # Import agents
 from rover_envs.envs.navigation.learning.skrl import get_agent  # noqa: E402
 from rover_envs.utils.config import parse_skrl_cfg  # noqa: E402
-
+from isaaclab.managers import DatasetExportMode  # noqa: E402
 
 def main():
     args_cli_seed = args_cli.seed if args_cli.seed is not None else random.randint(0, 100000000)
     env_cfg = parse_env_cfg(args_cli.task, device="cuda:0" if not args_cli.cpu else "cpu", num_envs=args_cli.num_envs)
+
+    if args_cli.dataset_name is not None:
+        env_cfg = configure_datarecorder(env_cfg, args_cli.dataset_dir, args_cli.dataset_name, args_cli.dataset_type)
+
+
     # key = agent name, value = path to config file
     experiment_cfg_file = gym.spec(args_cli.task).kwargs.get("skrl_cfgs")[args_cli.agent.upper()]
     experiment_cfg = parse_skrl_cfg(experiment_cfg_file)
