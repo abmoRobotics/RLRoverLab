43fc29fb409e0c8a398373e18a328254eb908873
abmoRobotics <antonbm2008@gmail.com>
2025-05-21 15:05:19 +0200

    Update task argument in eval script and modify RoverSceneCfg class for terrain configuration

--- examples/03_inference_pretrained/eval.py
+++ examples/03_inference_pretrained/eval.py
@@ -14,7 +14,7 @@ parser.add_argument("--video", action="store_true", default=False, help="Record 
 parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
 parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
-parser.add_argument("--task", type=str, default="AAURoverEnvSimple-v0", help="Name of the task.")
+parser.add_argument("--task", type=str, default="AAURoverEnv-v0", help="Name of the task.")
 parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
 parser.add_argument("--agent", type=str, default="PPO", help="Name of the agent.")
 parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint to resume training.")
