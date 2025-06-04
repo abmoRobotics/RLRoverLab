8106b07793cbdffd1399e2480b936202e3be0160
abmoRobotics <antonbm2008@gmail.com>
2025-06-04 23:15:03 +0200

    fix: Update best model path to use heightmap agent

--- rover_envs/envs/navigation/robots/aau_rover/__init__.py
+++ rover_envs/envs/navigation/robots/aau_rover/__init__.py
@@ -10,7 +10,7 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": env_cfg.AAURoverEnvCfg,
-        "best_model_path": f"{os.path.dirname(__file__)}/policies/best_agent2.pt",
+        "best_model_path": f"{os.path.dirname(__file__)}/policies/best_agent_heightmap.pt",
         "skrl_cfgs": {
             "PPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_ppo.yaml",
             "TRPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_trpo.yaml",
@@ -27,7 +27,7 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": env_cfg.AAURoverEnvCfgSimple,
-        "best_model_path": f"{os.path.dirname(__file__)}/policies/best_agent2.pt",
+        "best_model_path": f"{os.path.dirname(__file__)}/policies/best_agent_heightmap.pt",
         "skrl_cfgs": {
             "PPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_ppo.yaml",
             "TRPO": f"{os.path.dirname(__file__)}/../../learning/skrl/configs/rover_trpo.yaml",
