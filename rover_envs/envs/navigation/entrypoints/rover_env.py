19ff9f48e34bc24a3b2996fb260109fe636c47ce
abmoRobotics <antonbm2008@gmail.com>
2025-06-26 18:06:46 +0200

    refactor: Update import path for reset_root_state_rover and comment out its usage in RoverEnv

--- rover_envs/envs/navigation/entrypoints/rover_env.py
+++ rover_envs/envs/navigation/entrypoints/rover_env.py
@@ -9,7 +9,6 @@ from rover_envs.envs.navigation.rover_env_cfg import RoverEnvCfg
 VecEnvStepReturn = tuple[VecEnvObs, torch.Tensor,
                          torch.Tensor, torch.Tensor, dict]
 
-from mdp.randomizations import reset_root_state_rover
 class RoverEnv(ManagerBasedRLEnv):
     """ Rover environment.
 
@@ -21,9 +20,6 @@ class RoverEnv(ManagerBasedRLEnv):
         super().__init__(cfg, **kwargs)
         env_ids = torch.arange(self.num_envs, device=self.device)
 
-        reset_root_state_rover(
-            self, env_ids, self.scene.rover_asset_cfg, z_offset=0.5
-        )
 
         # Reset all environments
         # self._reset_idx(env_ids) 
