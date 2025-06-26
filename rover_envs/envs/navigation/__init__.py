86b7db58d165d31911b6a75a97b6b9c9542ab270
abmoRobotics <antonbm2008@gmail.com>
2025-06-26 14:42:14 +0200

    refactor: Remove commented-out code from navigation module

--- rover_envs/envs/navigation/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .entrypoints.rover_env import RoverEnv
-# from .rover_env_cfg import RoverEnvCfg
-# from .rover_env_no_obstacles_cfg import RoverEnvNoObstaclesCfg
-
-# __all__ = ["RoverEnvCfg", "RoverEnv", "RoverEnvNoObstaclesCfg"]
-# __all__ = ["RoverEnvCfg", "RoverEnv", "RoverEnvCamera"]
