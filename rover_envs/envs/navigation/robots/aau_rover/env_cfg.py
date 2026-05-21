07c423200a34f4d4918db00d82088d67c872212d
abmoRobotics <antonbm2008@gmail.com>
2026-05-21 20:34:35 +0200

    feat(aau_rover): add AAURoverEnvRGBDRawHD1080 environment and configuration

--- rover_envs/envs/navigation/robots/aau_rover/env_cfg.py
+++ rover_envs/envs/navigation/robots/aau_rover/env_cfg.py
@@ -8,6 +8,7 @@ from rover_envs.envs.navigation.rover_env_cfg import RoverEnvCfg
 from rover_envs.envs.navigation.rover_env_camera_cfg import (
     RoverCosmosEnvCfg,
     RoverRGBDRawEnvCfg,
+    RoverRGBDRawHD1080EnvCfg,
     RoverRGBDRawHD720EnvCfg,
     RoverRGBDRawTempEnvCfg,
     RoverRGBDRawWVGAEnvCfg,
@@ -144,6 +145,29 @@ class AAURoverRGBDRawEnvCfg(RoverRGBDRawEnvCfg):
 @configclass
 class AAURoverRGBDRawHD720EnvCfg(RoverRGBDRawHD720EnvCfg):
     """Configuration for the AAU rover with ZED2i HD720 RGB-D raw observations."""
+
+    def __post_init__(self):
+        super().__post_init__()
+        robot_cfg = AAU_ROVER_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        robot_cfg.spawn.visible = True
+        self.scene.robot = robot_cfg
+        self.actions.actions = mdp.AckermannActionCfg(
+            asset_name="robot",
+            wheelbase_length=0.849,
+            middle_wheel_distance=0.894,
+            rear_and_front_wheel_distance=0.77,
+            wheel_radius=0.1,
+            min_steering_radius=0.8,
+            steering_joint_names=[".*Steer_Revolute"],
+            drive_joint_names=[".*Drive_Continuous"],
+            offset=-0.0135
+        )
+        self.sim.dt = 1 / 45
+        self.decimation = 9
+
+@configclass
+class AAURoverRGBDRawHD1080EnvCfg(RoverRGBDRawHD1080EnvCfg):
+    """Configuration for the AAU rover with ZED2i HD1080 RGB-D raw observations."""
 
     def __post_init__(self):
         super().__post_init__()
