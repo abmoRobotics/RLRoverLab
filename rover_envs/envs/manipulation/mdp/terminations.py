2d71e1f16f792e0ebdb91e11572c7d3e1f23b137
Daizhi Liao <156556011+dafahaha@users.noreply.github.com>
2026-09-16 22:33:29 +0800

    Remove debug print statement from collision check
    Removed print statement for force matrix shape in collision check.

--- rover_envs/envs/manipulation/mdp/terminations.py
+++ rover_envs/envs/manipulation/mdp/terminations.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
+
 from typing import TYPE_CHECKING
+
 
 import torch
 from isaaclab.managers import SceneEntityCfg  # noqa: F401
 from isaaclab.sensors import ContactSensor
 
+
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
+
+
 
 
 def collision_with_table(env: ManagerBasedRLEnv, sensor_cfg: SceneEntityCfg, threshold: float) -> torch.Tensor:
@@ -17,12 +22,14 @@ def collision_with_table(env: ManagerBasedRLEnv, sensor_cfg: SceneEntityCfg, thr
     # Accessing the contact sensor and its data
     contact_sensor: ContactSensor = env.scene.sensors[sensor_cfg.name]
 
+
     # Reshape as follows (num_envs, num_bodies, 3)
-    print(contact_sensor.data.force_matrix_w.shape)
     force_matrix = contact_sensor.data.force_matrix_w.view(env.num_envs, -1, 3)
+
 
     # Calculating the force and returning true if it is above the threshold
     normalized_forces = torch.norm(force_matrix, dim=1)
     forces_active = torch.sum(normalized_forces, dim=-1) > 1
+
 
     return torch.where(forces_active, True, False)
